c9d1a04fb1cda75e504e6bf5a89cd72174f0b258
Prasad Patil <patil.prasad76@gmail.com>
2023-09-23 11:39:26 -0700

    Updated logo and cleaned the NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,6 @@
 import { HStack, Heading, Image } from "@chakra-ui/react";
 // import logo from "../assets/logo.webp";
-import logo from "../assets/logo2.png";
+import logo from "../assets/logo3.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
@@ -11,8 +11,10 @@ interface NavBarProps {
 const NavBar = ({ onSearch }: NavBarProps) => {
 	return (
 		<HStack padding="10px">
-			<Image src={logo} boxSize="60px" />
-			{/* <Heading fontSize="xl">GameHub</Heading> */}
+			<Image src={logo} boxSize="70px" padding="10px 0px 10px 10px" />
+			{/* <Heading fontWeight="800" fontSize="2xl" marginRight={3}>
+				GameHub
+			</Heading> */}
 			<SearchInput onSearch={onSearch} />
 			<ColorModeSwitch />
 		</HStack>
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,4 +1,4 @@
-import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { Box, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 
@@ -10,22 +10,24 @@ const SearchInput = ({ onSearch }: SearchInputProps) => {
 	const ref = useRef<HTMLInputElement>(null);
 
 	return (
-		<form
-			onSubmit={(event) => {
-				event.preventDefault();
-				if (ref.current) onSearch(ref.current.value);
-			}}
-		>
-			<InputGroup>
-				<InputLeftElement children={<BsSearch />} />
-				<Input
-					ref={ref}
-					borderRadius={20}
-					placeholder="Search titles..."
-					variant="filled"
-				/>
-			</InputGroup>
-		</form>
+		<Box width="100%" marginX="15px">
+			<form
+				onSubmit={(event) => {
+					event.preventDefault();
+					if (ref.current) onSearch(ref.current.value);
+				}}
+			>
+				<InputGroup>
+					<InputLeftElement children={<BsSearch />} />
+					<Input
+						ref={ref}
+						borderRadius={20}
+						placeholder="Search titles..."
+						variant="filled"
+					/>
+				</InputGroup>
+			</form>
+		</Box>
 	);
 };
 
