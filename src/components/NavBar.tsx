e671087c33ad6502449a83bf081ca719858eda84
Prasad Patil <patil.prasad76@gmail.com>
2023-11-08 15:48:30 -0800

    Fixed the navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,11 +3,14 @@ import { HStack, Heading, Image } from "@chakra-ui/react";
 import logo from "../assets/logo3.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
 	return (
 		<HStack padding="10px">
-			<Image src={logo} boxSize="70px" padding="10px 0px 10px 10px" />
+			<Link to="/">
+				<Image src={logo} boxSize="70px" padding="10px 0px 10px 10px" />
+			</Link>
 			{/* <Heading fontWeight="800" fontSize="2xl" marginRight={3}>
 				GameHub
 			</Heading> */}
